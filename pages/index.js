a77a4446f633b0227fc362df377f33a205ec8a36
Andy Tang <andyt@live.nl>
2019-07-10 19:27:12 +0200

    apollo client-server implemented

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,44 @@
-import Head from 'next/head';
+import Head from "next/head";
 import Link from "next/link";
+import material from "materialize-css/sass/materialize.scss";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 import Header from "../components/header";
-import material from 'materialize-css/sass/materialize.scss'
+import withData from "../lib/apollo";
+
+export const helloWorld = gql`
+  {
+    hello
+  }
+`;
 
 function Index() {
   return (
-    <main>
-      <Head>
-        <title>Sync Battle</title>
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
-      </Head>
-      <Header />
-      <section className="content">
-        <Link href="/about">
-          <a>Go to About Me</a>
-        </Link>
-      </section>
-      <style jsx>{`
-        ${material}
-        .content {
-
-        }
-      `}</style>
-    </main>
+    <Query query={helloWorld}>
+      {({ loading, error, data: { hello }, fetchMore }) => (
+        <main>
+          <Head>
+            <title>Sync Battle</title>
+            <link
+              href="https://fonts.googleapis.com/icon?family=Material+Icons"
+              rel="stylesheet"
+            />
+          </Head>
+          <Header />
+          <section className="content">
+            <Link href="/about">
+              <a>{ hello }</a>
+            </Link>
+          </section>
+          <style jsx>{`
+            ${material}
+            .content {
+            }
+          `}</style>
+        </main>
+      )}
+    </Query>
   );
 }
 
-export default Index;
+export default withData(Index);
--- server/index.js
+++ server/index.js
@@ -4,12 +4,21 @@ const typeDefs = gql`
   type Query {
     hello: String
   }
+
+  type Mutation {
+    saveName(name: String): String
+  }
 `;
 
 const resolvers = {
   Query: {
     hello: (root, args, context) => {
       return "Hello world! It's your boy, how far now unicodeveloper";
+    }
+  },
+  Mutation: {
+    saveName: (root, args, context) => {
+      return `Hello ${args.name}! Your name has been saved!`;
     }
   }
 };
