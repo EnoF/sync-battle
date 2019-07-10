16b73949225134cbd9237b91ab3f7a06ca4cf2f6
Andy Tang <andyt@live.nl>
2019-07-10 18:13:39 +0200

    added meterialize css

--- components/header.js
+++ components/header.js
@@ -1,7 +1,36 @@
+import Head from 'next/head';
+import Link from "next/link";
+import material from 'materialize-css/sass/materialize.scss'
+
 function Header() {
   return (
     <header>
-      <h1>Next.js Example on Now 2.0</h1>
+      <Head>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+      </Head>
+      <nav>
+        <div className="nav-wrapper">
+          <Link href="/">
+            <a>
+              <i className="material-icons logo">terrain</i>
+            </a>
+          </Link>
+          <ul className="right hide-on-med-and-down">
+            <li>
+              <Link href="/about">
+                <a>About</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <style jsx>{`
+        .logo {
+          display: inline-block;
+          margin-left: 24px;
+        }
+        ${material}
+      `}</style>
     </header>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,27 @@
+import Head from 'next/head';
 import Link from "next/link";
 import Header from "../components/header";
+import material from 'materialize-css/sass/materialize.scss'
 
 function Index() {
   return (
     <main>
+      <Head>
+        <title>Sync Battle</title>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+      </Head>
       <Header />
-      <section>
+      <section className="content">
         <Link href="/about">
           <a>Go to About Me</a>
         </Link>
       </section>
+      <style jsx>{`
+        ${material}
+        .content {
+
+        }
+      `}</style>
     </main>
   );
 }
