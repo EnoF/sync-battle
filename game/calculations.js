d76c333d51cdaa1b358ed5e52a86e4ec0c599aab
Andy Tang <andyt@live.nl>
2019-12-09 19:18:26 +0100

    Use current stamina in calculations

--- game/calculations.js
+++ game/calculations.js
@@ -1,13 +1,14 @@
 const getHp = player => player.hp
+const getStamina = player => player.stamina
 export const calculate = ({ p1, p2 }) => {
   return {
     p1: {
       hp: getHp(p1) - 1,
-      stamina: 4,
+      stamina: getStamina(p1) - 1,
     },
     p2: {
       hp: getHp(p2) - 1,
-      stamina: 4,
+      stamina: getStamina(p2) - 1,
     },
   }
 }
--- game/calculations.specs.js
+++ game/calculations.specs.js
@@ -8,28 +8,32 @@ describe('Calculations', () => {
   getPermutations({
     p1Hp: [10, 9],
     p1Move: ['attack'],
-    p1Stamina: [5],
+    p1Stamina: [5, 4],
     p2Hp: [10, 9],
     p2Move: ['attack'],
-    p2Stamina: [5],
+    p2Stamina: [5, 4],
   }).forEach(({ p1Hp, p1Move, p1Stamina, p2Hp, p2Move, p2Stamina }) => {
-    describe(`when player one uses ${p1Move} with ${p1Hp} left`, () => {
-      describe(`and player two uses ${p2Move} with ${p2Hp} left`, () => {
-        it(`should reduce player two's hp`, function() {
-          this.verifyAsJSON(
-            calculate({
-              p1: {
-                hp: p1Hp,
-                stamina: p1Stamina,
-                move: p1Move,
-              },
-              p2: {
-                hp: p2Hp,
-                stamina: p2Stamina,
-                move: p2Move,
-              },
+    describe(`when player one uses ${p1Move}`, () => {
+      describe(`and has ${p1Hp}hp with ${p1Stamina}stamina`, () => {
+        describe(`and player two uses ${p2Move}`, () => {
+          describe(`and has ${p2Hp}hp with ${p2Stamina}stamina`, () => {
+            it(`should reduce player two's hp`, function() {
+              this.verifyAsJSON(
+                calculate({
+                  p1: {
+                    hp: p1Hp,
+                    stamina: p1Stamina,
+                    move: p1Move,
+                  },
+                  p2: {
+                    hp: p2Hp,
+                    stamina: p2Stamina,
+                    move: p2Move,
+                  },
+                })
+              )
             })
-          )
+          })
         })
       })
     })
