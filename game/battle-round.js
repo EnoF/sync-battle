c21513c51c66e38f4b24401e65c666da31c76872
Andy Tang <andyt@live.nl>
2019-12-16 08:58:47 +0100

    Add Battle round specs - WIP

---
+++ game/battle-round.js
@@ -0,0 +1,3 @@
+import { calculate } from './calculations'
+
+export const round = ({ p1, p2 }) => {}
---
+++ game/battle-round.specs.js
@@ -0,0 +1,10 @@
+import { round } from './battle-round'
+
+describe('Battle round', () => {
+  describe('when the round is over', () => {
+    it('should replenish some of the stamina to the players')
+  })
+  describe('when one of the players hp is reduced to zero', () => {
+    it('should stop executing any other moves')
+  })
+})
