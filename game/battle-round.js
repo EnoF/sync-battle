7c0b538952f05182a6de642a32e24a30010b5986
Andy Tang <andyt@live.nl>
2019-12-28 15:50:45 +0100

    Add stamina after 5 moves

--- game/battle-round.js
+++ game/battle-round.js
@@ -1,3 +1,34 @@
 import { calculate } from './calculations'
 
-export const round = ({ p1, p2 }) => {}
+const getMoves = player => player.moves
+const getP1 = players => players.p1
+const getP2 = players => players.p2
+const getStamina = player => player.stamina
+const appendMove = move => player => ({ ...player, move })
+const sum = x => y => x + y
+const addStamina = player => ({
+  ...player,
+  stamina: player |> getStamina |> sum(5),
+})
+const addStaminaToPlayers = players => ({
+  p1: players |> getP1 |> addStamina,
+  p2: players |> getP2 |> addStamina,
+})
+const removeMoves = ({ moves, ...player }) => ({ ...player })
+const removeMovesFromPlayers = players => ({
+  p1: players |> getP1 |> removeMoves,
+  p2: players |> getP2 |> removeMoves,
+})
+const calculateRounds = players => {
+  const p1Moves = players |> getP1 |> getMoves
+  const p2Moves = players |> getP2 |> getMoves
+  return p1Moves.reduce(
+    (calculatedPlayers, move, index) =>
+      calculate({
+        p1: calculatedPlayers |> getP1 |> appendMove(move),
+        p2: calculatedPlayers |> getP2 |> appendMove(p2Moves[index]),
+      }),
+    players
+  )
+}
+export const round = players => players |> calculateRounds |> addStaminaToPlayers |> removeMovesFromPlayers
--- game/battle-round.specs.js
+++ game/battle-round.specs.js
@@ -1,8 +1,51 @@
+import 'mocha'
+import { expect } from 'chai'
 import { round } from './battle-round'
 
 describe('Battle round', () => {
+  const p1 = { hp: 10, stamina: 5 }
+  const p2 = { hp: 10, stamina: 5 }
   describe('when the round is over', () => {
-    it('should replenish some of the stamina to the players')
+    const fiveAttacks = [
+      {
+        type: 'attack',
+      },
+      {
+        type: 'attack',
+      },
+      {
+        type: 'attack',
+      },
+      {
+        type: 'attack',
+      },
+      {
+        type: 'attack',
+      },
+    ]
+    it('should replenish some of the stamina to the players', () => {
+      expect(
+        round({
+          p1: {
+            ...p1,
+            moves: fiveAttacks,
+          },
+          p2: {
+            ...p2,
+            moves: fiveAttacks,
+          },
+        })
+      ).to.deep.equal({
+        p1: {
+          hp: 5,
+          stamina: 5,
+        },
+        p2: {
+          hp: 5,
+          stamina: 5,
+        },
+      })
+    })
   })
   describe('when one of the players hp is reduced to zero', () => {
     it('should stop executing any other moves')
