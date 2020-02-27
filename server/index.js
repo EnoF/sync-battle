d9a7d144372eb4cbda27b9cfb04172d2652d385e
Andy Tang <andyt@live.nl>
2020-02-27 09:32:04 +0100

    Add Player model to graphql schema

--- server/index.js
+++ server/index.js
@@ -1,27 +1,49 @@
-const { ApolloServer, gql } = require("apollo-server-micro");
+const { round } = require('../game/battle-round')
+
+const { ApolloServer, gql } = require('apollo-server-micro')
 
 const typeDefs = gql`
+  type Move {
+    type: String
+    power: Int
+  }
+
+  type Players {
+    p1: Player
+    p2: Player
+  }
+
+  type Player {
+    hp: Int
+    stamina: Int
+    moves: [Move]
+  }
+
   type Query {
     hello: String
   }
 
   type Mutation {
     login(userName: String, password: String): String
+    calculate(players: Players): Players
   }
-`;
+`
 
 const resolvers = {
   Query: {
     hello: (root, args, context) => {
-      return "Hello world! It's your boy, how far now unicodeveloper";
-    }
+      return "Hello world! It's your boy, how far now unicodeveloper"
+    },
   },
   Mutation: {
     login: (root, { userName }, context) => {
-      return `Hello ${userName}! Welcome back!`;
-    }
-  }
-};
+      return `Hello ${userName}! Welcome back!`
+    },
+    calculate: (root, { players }) => {
+      return round(players)
+    },
+  },
+}
 
 const server = new ApolloServer({
   typeDefs,
