9eae9616e7be5f393743e67be7e8ba1a98182474
Andy Tang <andyt@live.nl>
2020-03-02 07:59:50 +0100

    Show calculated values

--- pages/battle.js
+++ pages/battle.js
@@ -3,22 +3,17 @@ import gql from 'graphql-tag'
 import withData from '../lib/apollo'
 import Header from '../components/header'
 import Form from '../components/form'
+import { getHp, getStamina } from '../game/player'
+import { getP1, getP2 } from '../game/players'
 
 export const CALCULATE_ROUND = gql`
   mutation NewRound($p1Hp: Int!, $p1Stamina: Int!, $p1Move: String, $p2Hp: Int!, $p2Stamina: Int!, $p2Move: String) {
-    calculate(players: { p1: {
-      hp: $p1Hp,
-      stamina: $p1Stamina,
-      moves: [{
-        type: $p1Move
-      }],
-    }, p2: {
-      hp: $p2Hp,
-      stamina: $p2Stamina,
-      moves: [{
-        type: $p2Move
-      }],
-    } }) {
+    calculate(
+      players: {
+        p1: { hp: $p1Hp, stamina: $p1Stamina, moves: [{ type: $p1Move }] }
+        p2: { hp: $p2Hp, stamina: $p2Stamina, moves: [{ type: $p2Move }] }
+      }
+    ) {
       p1 {
         hp
         stamina
@@ -31,6 +26,23 @@ export const CALCULATE_ROUND = gql`
   }
 `
 
+const getDefaultPlayerData = () => ({
+  p1: {
+    hp: 10,
+    stamina: 5,
+  },
+  p2: {
+    hp: 10,
+    stamina: 5,
+  },
+})
+const getPlayerData = (data = {}) => data.calculate || getDefaultPlayerData()
+const getP1Data = data => data |> getPlayerData |> getP1
+const getP2Data = data => data |> getPlayerData |> getP2
+const getP1Hp = data => data |> getP1Data |> getHp
+const getP1Stamina = data => data |> getP1Data |> getStamina
+const getP2Hp = data => data |> getP2Data |> getHp
+const getP2Stamina = data => data |> getP2Data |> getStamina
 const Battle = () => (
   <main>
     <Header />
@@ -40,32 +52,32 @@ const Battle = () => (
     <section>
       <h1>Round</h1>
       <Form action="/battle" mutation={CALCULATE_ROUND}>
-        {({ loading }) => (
+        {({ loading, data }) => (
           <>
             {!!loading && <div>Calculating...</div>}
             <label>
               hp:
-              <input type="number" name="p1Hp" value="10" disabled={loading} />
+              <input type="number" readOnly name="p1Hp" value={data |> getP1Hp} disabled={loading} />
             </label>
             <label>
               stamina:
-              <input type="number" name="p1Stamina" value="5" disabled={loading} />
+              <input type="number" readOnly name="p1Stamina" value={data |> getP1Stamina} disabled={loading} />
             </label>
             <label>
               move:
-              <input type="text" name="p1Move" value="attack" disabled={loading} />
+              <input type="text" name="p1Move" defaultValue="attack" disabled={loading} />
             </label>
             <label>
               hp:
-              <input type="number" name="p2Hp" value="10" disabled={loading} />
+              <input type="number" readOnly name="p2Hp" value={data |> getP2Hp} disabled={loading} />
             </label>
             <label>
               stamina:
-              <input type="number" name="p2Stamina" value="5" disabled={loading} />
+              <input type="number" readOnly name="p2Stamina" value={data |> getP2Stamina} disabled={loading} />
             </label>
             <label>
               move:
-              <input type="text" name="p2Move" value="attack" disabled={loading} />
+              <input type="text" name="p2Move" defaultValue="attack" disabled={loading} />
             </label>
             <button type="submit" disabled={loading}>
               calculate
